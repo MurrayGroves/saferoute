131de1929889ad214ca64236a2d945e61cf7f7aa
Murray Groves <github@murraygrov.es>
2024-02-25 21:01:36 +0000

    Search for from/to

--- saferoute/src/App.tsx
+++ saferoute/src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { RefObject } from "react";
 
 import './App.css';
 import { Input, Paper, TextField } from '@mui/material';
 import {makeStyles} from '@mui/styles';
 
-import Autocomplete from 'react-google-autocomplete';
+import Autocomplete, { usePlacesWidget } from 'react-google-autocomplete';
 import { ClickableMap } from "./ClickableMap";
 import { LatLong } from "./types";
 
@@ -14,13 +14,48 @@ function App() {
   const [start, setStart] = React.useState<LatLong>();
   const [end, setEnd] = React.useState<LatLong>();
 
+  let refs = usePlacesWidget({
+    apiKey: process.env.REACT_APP_GOOGLE_KEY,
+    onPlaceSelected: (place) => {
+      if(place.geometry?.location?.lat() === undefined) return console.log('no location')
+      if(place.geometry?.location?.lng() === undefined) return console.log('no location')
+      let loc: LatLong = [place.geometry?.location?.lat(), place.geometry?.location?.lng()]
+      setStart(loc)
+    },
+    options: {types: []}
+  })
+
+  const fromRef = refs.ref;
+
+  refs = usePlacesWidget({
+    apiKey: process.env.REACT_APP_GOOGLE_KEY,
+    onPlaceSelected: (place) => {
+      if(place.geometry?.location?.lat() === undefined) return console.log('no location')
+      if(place.geometry?.location?.lng() === undefined) return console.log('no location')
+      let loc: LatLong = [place.geometry?.location?.lat(), place.geometry?.location?.lng()]
+      setEnd(loc)
+    },
+    options: { types: [] }
+  })
+
+  const toRef = refs.ref;
+
   return (
     <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
-      <Paper sx={{borderRadius: "25px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute', zIndex: '100'}}>
-        <Autocomplete apiKey={process.env.REACT_APP_GOOGLE_KEY} options={{types: []}}
+      <Paper sx={{borderRadius: "10px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute', top: '1%', zIndex: '100'}}>
+        <input ref={fromRef as unknown as RefObject<HTMLInputElement>} placeholder="From"
           style={{border: 'none', width: '90%', height: '100%', fontSize: '120%'
-          , backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}} placeholder="Search" />
+          , backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}}
+          />
       </Paper>
+
+      <Paper sx={{borderRadius: "10px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute', top: '7%', zIndex: '100'}}>
+        <input ref={toRef as unknown as RefObject<HTMLInputElement>} placeholder="To"
+          style={{border: 'none', width: '90%', height: '100%', fontSize: '120%'
+          , backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}}
+          />
+      </Paper>
+      
 
       <ClickableMap
         start={start}
