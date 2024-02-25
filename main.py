7a32d8d38d1a55c6a13e7e254c7904b89e469c35
AniBoja <aniboja26@gmail.com>
2024-02-25 15:31:49 +0000

    actually makes request

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ class OpenTripPlanner():
                  date=str(datetime.datetime.now().strftime("%I:%M:%p")), 
                  arriveBy="FALSE"):
         
+        
         request = self.host + "/otp/routers/default/plan?fromPlace=" + source + "&toPlace=" + destination + "&time=" + time + "&date=" + date + "&MODE=" + mode + "&arriveBy=" + arriveBy
-        return request
-
+        response = requests.get(request)
+        return response
 
 
         
