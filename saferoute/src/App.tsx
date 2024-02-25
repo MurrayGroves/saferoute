d9eaaacf295d1c2586448a7ff4f1890d63bd775e
Murray Groves <github@murraygrov.es>
2024-02-25 17:53:16 +0000

    add search bar

--- saferoute/src/App.tsx
+++ saferoute/src/App.tsx
@@ -2,28 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import BaseMap from '@opentripplanner/base-map'
 
-import Autocomplete from '@mui/material/Autocomplete';
-
 import './App.css';
-import { TextField } from '@mui/material';
+import { Input, Paper, TextField } from '@mui/material';
 import {makeStyles} from '@mui/styles';
 
-const useStyles = makeStyles((theme: any) => ({
-  root: {
-    "& .MuiFilledInput-root": {
-      background: "rgb(232, 241, 250)"
-    }
-  }
-}));
+import Autocomplete from 'react-google-autocomplete';
 
 function App() {
-  return (
-    <div className="App">
-      <Autocomplete renderInput={(params) => <TextField {...params} label="Movie"/>} options={[]} freeSolo={true}
-        style={{margin: '5%'}}
-      />
+  const [search, setSearch] = React.useState('');
+  const [mapsValue, setMapsValue] = React.useState(null);
 
-      <BaseMap center={[51.49350, -2.57634]} style={{ position: 'absolute', top: '0%', height: '70vh', zIndex: '-1' }} mapLibreProps={{ attributionControl: false }}>
+  return (
+    <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
+      <Paper sx={{borderRadius: "25px", width: '90%', marginTop: '5%', height: '5vh'}}>
+        <Autocomplete apiKey={process.env.REACT_APP_GOOGLE_KEY} options={{types: []}}
+          style={{border: 'none', width: '90%', height: '100%', fontSize: '120%', backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}} placeholder="Search" />
+      </Paper>
+
+      <BaseMap center={[51.49350, -2.57634]} style={{ position: 'absolute', top: '0%', height: '100%', zIndex: '-1' }} mapLibreProps={{ attributionControl: false }}>
 
       </BaseMap>
     </div>
