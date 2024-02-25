4067043aefe16b5beb50f3299ef9616f7b76bd80
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-02-25 21:48:48 +0000

    manually fix any merging weirdness

--- saferoute/src/App.tsx
+++ saferoute/src/App.tsx
@@ -1,25 +1,20 @@
 import React, { useEffect, RefObject } from "react";
-import { Layer, Source, Map } from "react-map-gl/maplibre";
-import Autocomplete from "@mui/material/Autocomplete";
-import './App.css';
-import { Input, Paper, TextField } from '@mui/material';
-import {makeStyles} from '@mui/styles';
+import { Layer, Source } from "react-map-gl/maplibre";
 
-import Autocomplete, { usePlacesWidget } from 'react-google-autocomplete';
+import "./App.css";
+import { Paper, TextField } from "@mui/material";
+
+import Autocomplete, { usePlacesWidget } from "react-google-autocomplete";
 import { ClickableMap } from "./ClickableMap";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { LatLong, PatchedItinerary, PathFindingResponse } from "./types";
 import { routeToFeature } from "./routeUtils";
-import type { LineLayer } from "react-map-gl/maplibre";
-import type { FeatureCollection, Feature } from "geojson";
-import { LatLong } from "./types";
 
 const pathFindingURL =
   "http://100.86.237.92:8080/otp/routers/default/plan?fromPlace={from}&toPlace={to}}&time=2:05pm&date=25-02-24&MODE=BICYCLE&arriveBy=FALSE&showIntermediateStops=true&wheelchair=FALSE";
 
-
 function App() {
-  const [search, setSearch] = React.useState('');
+  const [search, setSearch] = React.useState("");
   const [mapsValue, setMapsValue] = React.useState(null);
   const [start, setStart] = React.useState<LatLong>();
   const [end, setEnd] = React.useState<LatLong>();
@@ -27,6 +22,7 @@ function App() {
 
   useEffect(() => {
     if (start && end) {
+    
       fetch(
         pathFindingURL
           .replace("{from}", encodeURIComponent(start.join(",")))
@@ -35,59 +31,102 @@ function App() {
         .then((response) => response.json() as Promise<PathFindingResponse>)
         .then((response) => {
           setRoute(response.plan.itineraries);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
         });
     }
   }, [start, end]);
-    
+
   let refs = usePlacesWidget({
     apiKey: process.env.REACT_APP_GOOGLE_KEY,
     onPlaceSelected: (place) => {
-      if(place.geometry?.location?.lat() === undefined) return console.log('no location')
-      if(place.geometry?.location?.lng() === undefined) return console.log('no location')
-      let loc: LatLong = [place.geometry?.location?.lat(), place.geometry?.location?.lng()]
-      setStart(loc)
+      if (place.geometry?.location?.lat() === undefined)
+        return console.log("no location");
+      if (place.geometry?.location?.lng() === undefined)
+        return console.log("no location");
+      let loc: LatLong = [
+        place.geometry?.location?.lat(),
+        place.geometry?.location?.lng(),
+      ];
+      setStart(loc);
     },
-    options: {types: []}
-  })
+    options: { types: [] },
+  });
 
   const fromRef = refs.ref;
 
   refs = usePlacesWidget({
     apiKey: process.env.REACT_APP_GOOGLE_KEY,
     onPlaceSelected: (place) => {
-      if(place.geometry?.location?.lat() === undefined) return console.log('no location')
-      if(place.geometry?.location?.lng() === undefined) return console.log('no location')
-      let loc: LatLong = [place.geometry?.location?.lat(), place.geometry?.location?.lng()]
-      setEnd(loc)
+      if (place.geometry?.location?.lat() === undefined)
+        return console.log("no location");
+      if (place.geometry?.location?.lng() === undefined)
+        return console.log("no location");
+      let loc: LatLong = [
+        place.geometry?.location?.lat(),
+        place.geometry?.location?.lng(),
+      ];
+      setEnd(loc);
     },
-    options: { types: [] }
-  })
+    options: { types: [] },
+  });
 
   const toRef = refs.ref;
 
-
   return (
-    <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
-            <Autocomplete
-        renderInput={(params) => <TextField {...params} label="Movie" />}
-        options={[]}
-        freeSolo={true}
-      />
-
-      <Paper sx={{borderRadius: "10px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute', top: '1%', zIndex: '100'}}>
-        <input ref={fromRef as unknown as RefObject<HTMLInputElement>} placeholder="From"
-          style={{border: 'none', width: '90%', height: '100%', fontSize: '120%'
-          , backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}}
-          />
+    <div className="App" style={{ justifyContent: "center", display: "flex" }}>
+      <Paper
+        sx={{
+          borderRadius: "10px",
+          width: "90%",
+          marginTop: "5%",
+          height: "5vh",
+          position: "absolute",
+          top: "1%",
+          zIndex: "100",
+        }}
+      >
+        <input
+          ref={fromRef as unknown as RefObject<HTMLInputElement>}
+          placeholder="From"
+          style={{
+            border: "none",
+            width: "90%",
+            height: "100%",
+            fontSize: "120%",
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            outline: "none",
+            borderColor: "transparent",
+          }}
+        />
       </Paper>
 
-      <Paper sx={{borderRadius: "10px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute', top: '7%', zIndex: '100'}}>
-        <input ref={toRef as unknown as RefObject<HTMLInputElement>} placeholder="To"
-          style={{border: 'none', width: '90%', height: '100%', fontSize: '120%'
-          , backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}}
-          />
+      <Paper
+        sx={{
+          borderRadius: "10px",
+          width: "90%",
+          marginTop: "5%",
+          height: "5vh",
+          position: "absolute",
+          top: "7%",
+          zIndex: "100",
+        }}
+      >
+        <input
+          ref={toRef as unknown as RefObject<HTMLInputElement>}
+          placeholder="To"
+          style={{
+            border: "none",
+            width: "90%",
+            height: "100%",
+            fontSize: "120%",
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            outline: "none",
+            borderColor: "transparent",
+          }}
+        />
       </Paper>
-      
 
       <ClickableMap
         start={start}
@@ -98,6 +137,7 @@ function App() {
         initialViewState={{ latitude: 51.4935, longitude: -2.57634, zoom: 12 }}
         attributionControl={false}
         style={{ width: "100vw", height: "100vh" }}
+      >
         {route && (
           <Source id="route" type="geojson" data={routeToFeature(route)}>
             <Layer
