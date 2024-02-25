3d614ee3b2367d170adee445a1e173888ebfde97
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-02-25 17:33:01 +0000

    we are so back

--- saferoute/src/App.tsx
+++ saferoute/src/App.tsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import BaseMap from '@opentripplanner/base-map'
+import React from "react";
 
-import Autocomplete from '@mui/material/Autocomplete';
+import Autocomplete from "@mui/material/Autocomplete";
 
-import './App.css';
-import { TextField } from '@mui/material';
-import {makeStyles} from '@mui/styles';
+import "./App.css";
+import { TextField } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { ClickableMap } from "./ClickableMap";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { LatLong } from "./types";
 
 const useStyles = makeStyles((theme: any) => ({
   root: {
     "& .MuiFilledInput-root": {
-      background: "rgb(232, 241, 250)"
-    }
-  }
+      background: "rgb(232, 241, 250)",
+    },
+  },
 }));
 
 function App() {
+  const [start, setStart] = React.useState<LatLong>();
+  const [end, setEnd] = React.useState<LatLong>();
   return (
     <div className="App">
-      <Autocomplete renderInput={(params) => <TextField {...params} label="Movie"/>} options={[]} freeSolo={true}
-        style={{margin: '5%'}}
+      <Autocomplete
+        renderInput={(params) => <TextField {...params} label="Movie" />}
+        options={[]}
+        freeSolo={true}
       />
 
-      <BaseMap center={[51.49350, -2.57634]} style={{ position: 'absolute', top: '0%', height: '70vh', zIndex: '-1' }} mapLibreProps={{ attributionControl: false }}>
-
-      </BaseMap>
+      <ClickableMap
+        start={start}
+        end={end}
+        setStart={setStart}
+        setEnd={setEnd}
+        initialViewState={{ latitude: 51.4935, longitude: -2.57634, zoom: 12 }}
+        attributionControl={false}
+        style={{ width: "100vw", height: "100vw" }}
+      ></ClickableMap>
     </div>
   );
 }
