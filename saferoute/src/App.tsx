fac2e0c9edf9b770990381736081a63d0d7bb121
Murray Groves <github@murraygrov.es>
2024-02-25 18:06:12 +0000

    Fix styling

--- saferoute/src/App.tsx
+++ saferoute/src/App.tsx
@@ -5,14 +5,18 @@ import { Input, Paper, TextField } from '@mui/material';
 import {makeStyles} from '@mui/styles';
 
 import Autocomplete from 'react-google-autocomplete';
+import { ClickableMap } from "./ClickableMap";
+import { LatLong } from "./types";
 
 function App() {
   const [search, setSearch] = React.useState('');
   const [mapsValue, setMapsValue] = React.useState(null);
+  const [start, setStart] = React.useState<LatLong>();
+  const [end, setEnd] = React.useState<LatLong>();
 
   return (
     <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
-      <Paper sx={{borderRadius: "25px", width: '90%', marginTop: '5%', height: '5vh'}}>
+      <Paper sx={{borderRadius: "25px", width: '90%', marginTop: '5%', height: '5vh', position: 'absolute'}}>
         <Autocomplete apiKey={process.env.REACT_APP_GOOGLE_KEY} options={{types: []}}
           style={{border: 'none', width: '90%', height: '100%', fontSize: '120%', backgroundColor: 'rgba(0, 0, 0, 0)', outline: 'none', borderColor: 'transparent'}} placeholder="Search" />
       </Paper>
@@ -24,7 +28,7 @@ function App() {
         setEnd={setEnd}
         initialViewState={{ latitude: 51.4935, longitude: -2.57634, zoom: 12 }}
         attributionControl={false}
-        style={{ width: "100vw", height: "100vw" }}
+        style={{ width: "100vw", height: "100vh", zIndex: '-1' }}
       ></ClickableMap>
     </div>
   );
