5be89a05b7d86297c38c1c8497a3fdb75a43a003
Murray Groves <github@murraygrov.es>
2024-02-26 05:17:17 +0000

    remove an insane number of files

--- saferoute/node_modules/@opentripplanner/trip-form/node_modules/@styled-icons/bootstrap/OneHundredTwentyThree/OneHundredTwentyThree.d.ts
+++
@@ -1,6 +0,0 @@
-import { StyledIcon } from '@styled-icons/styled-icon';
-export declare const OneHundredTwentyThree: StyledIcon;
-export declare const OneHundredTwentyThreeDimensions: {
-    height: number;
-    width: number;
-};
